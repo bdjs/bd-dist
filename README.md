0b4165090947e9dfabe2ce952e5ba9c95532ed6c
jiashunming <jiashunming@outlook.com>
2018-04-19 19:57:12 +0800

    update: first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# bd-dist
+# bd-static
 bd.js static middleware
+
+```shell
+npm install --save bd-dist
+```
+
+`koa1.x` supports: [https://www.npmjs.com/package/bd-static](https://www.npmjs.com/package/bd-static)
+
+## Updated
+
+use `koa2.x`, faster and stronger.
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * bd-dist - index.js
+ * Created by jiasm on 18/04/19
+ */
+
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+const resolve = require('path').resolve
+const assert = require('assert')
+const send = require('koa-send')
+
+/**
+ * Expose `serve()`.
+ */
+
+const routes = {}
+
+/**
+ * Serve static files from `root`.
+ *
+ * @param {String} root
+ * @param {Object} [opts]
+ * @return {Function}
+ * @api public
+ */
+
+module.exports = function serve (root, route = '/', opts = {}) {
+  assert(root, 'root directory is required to serve files')
+
+  // options
+  opts.root = resolve(root)
+  opts.index = opts.index || 'index.html'
+
+  routes[route] = opts
+  return async function serve (next) {
+    await next()
+    var route = this.path.split('/')[1] || '/'
+    if (this.method !== 'HEAD' && this.method !== 'GET') return
+    // response is already handled
+    if (this.body !== null || this.status !== 404) return
+
+    await send(this, this.path.slice(1 + route.length) || '/', routes[route])
+  }
+}
